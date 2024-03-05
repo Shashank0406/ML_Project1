1e28be9425fe72c6bf90ee73b3b4c206822b438d
shashank kurakula <shashankkurakula.417@gmail.com>
2024-03-05 02:00:50 -0500

    Data Ingestion

--- test.py
+++ test.py
@@ -1,13 +1,4 @@
-import os
+import pandas as pd
 
-print(os.getcwd())
-
-def check_directory_permissions(directory_path):
-    if os.access(directory_path, os.W_OK):
-        print(f"The directory '{directory_path}' has write permissions.")
-    else:
-        print(f"The directory '{directory_path}' does not have write permissions.")
-
-# Example usage
-directory_to_check = os.getcwd()
-check_directory_permissions(directory_to_check)
+df=pd.read_csv('S:\Project1\StudentsPerformance.csv')
+print(df)
